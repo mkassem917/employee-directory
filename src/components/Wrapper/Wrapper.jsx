c149503fd87ee11d9733ccbf98049d7120a5c611
Moe Kassem <74279957+mkassem917@users.noreply.github.com>
2021-04-20 10:54:23 -0400

    added API and Wrapper, started components

---
+++ src/components/Wrapper/Wrapper.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import "./Wrapper.css";
+
+function Wrapper({children}) {
+    return (
+        
+            <main className="wrapper">{children}</main>;
+        
+    );
+};
+
+export default Wrapper;
